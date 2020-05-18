1c15ca697cdd4456f874ee78226086882e2a73bc
Tiago Reis <nuno-tiago-reis@outlook.pt>
2020-05-18 16:59:08 +0100

    Initial Commit.

---
+++ Memento/Memento.Shared/Configuration/ApplicationSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Memento.Shared.Configuration
+{
+	/// <summary>
+	/// Implements the application settings.
+	/// </summary>
+	public sealed class ApplicationSettings
+	{
+		// Nothing to do here.
+	}
+}
---
+++ Memento/Memento.Shared/Routing/SlashParameterTransformer.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Routing;
+using System.Text.RegularExpressions;
+
+namespace Memento.Shared.Routing
+{
+	/// <summary>
+	/// Implements a parameter transformer that separates words by using slashes.
+	/// </summary>
+	/// 
+	/// <seealso cref="IOutboundParameterTransformer" />
+	public sealed class SlashParameterTransformer : IOutboundParameterTransformer
+	{
+		#region [Methods]
+		/// <summary>
+		/// Transforms the outbound parameter values by splitting whenever a capital letter occurs using a slash.
+		/// </summary>
+		/// 
+		/// <param name="value">The value.</param>
+		public string TransformOutbound(object value)
+		{
+			if (value == null)
+			{
+				return null;
+			}
+
+			var parameter = string.Empty;
+
+			var regex = new Regex(@"(?<=[A-Z])(?=[A-Z][a-z]) | (?<=[^A-Z])(?=[A-Z]) | (?<=[A-Za-z])(?=[^A-Za-z])", RegexOptions.IgnorePatternWhitespace);
+			var regexTokens = regex.Split(parameter.ToString());
+
+			foreach (var regexToken in regexTokens)
+			{
+				if (regexToken.EndsWith("y") || regexToken.EndsWith("Y"))
+				{
+					parameter += $"{regexToken.Substring(0, regexToken.Length - 1)}ies/";
+				}
+				else
+				{
+					parameter += $"{regexToken}s/";
+				}
+			}
+
+			return parameter.ToLower();
+		}
+		#endregion
+	}
+}
