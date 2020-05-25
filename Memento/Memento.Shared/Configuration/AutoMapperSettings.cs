b3bc0f3c4d046be4a70a8d693a1290331059f76b
Tiago Reis <nuno-tiago-reis@outlook.pt>
2020-05-25 18:25:25 +0100

    AutoMapper:
    Added the necessary maps for the shared types.

---
+++ Memento/Memento.Shared/Configuration/AutoMapperSettings.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Memento.Shared.Pagination;
+
+namespace Memento.Movies.Shared.Configuration
+{
+	/// <summary>
+	/// Implements the 'AutoMapper' settings.
+	/// </summary>
+	/// 
+	/// <seealso cref="Profile" />
+	public abstract class AutoMapperSettings : Profile
+	{
+		#region [Constructor]
+		/// <summary>
+		/// Initializes a new instance of the <see cref="AutoMapperSettings"/> class.
+		/// </summary>
+		public AutoMapperSettings()
+		{
+			this.CreateMappings();
+		}
+		#endregion
+
+		#region [Methods]
+		/// <summary>
+		/// Creates the mapping configurations.
+		/// </summary>
+		protected virtual void CreateMappings()
+		{
+			#region [Pagination]
+			// Pagination
+			this.CreateMap(typeof(Page<>), typeof(Page<>));
+			#endregion
+		}
+		#endregion
+	}
+}
