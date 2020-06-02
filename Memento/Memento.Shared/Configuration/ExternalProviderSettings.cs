62ecf0d8a07cbe7aa4f01408d82058f327fdc7eb
Tiago Reis <nuno-tiago-reis@outlook.pt>
2020-06-02 21:33:55 +0100

    ExternalProviderSettings:
    Added the ExternalProviderSettings class.

---
+++ Memento/Memento.Shared/Configuration/ExternalProviderSettings.cs
@@ -0,0 +1,20 @@
+﻿namespace Memento.Shared.Configuration
+{
+	/// <summary>
+	/// Implements the 'ExternalProvider' options.
+	/// </summary>
+	public sealed class ExternalProviderOptions
+	{
+		#region [Properties]
+		/// <summary>
+		/// Gets or sets the api key.
+		/// </summary>
+		public string ApiKey { get; set; }
+
+		/// <summary>
+		/// Gets or sets the api secret.
+		/// </summary>
+		public string ApiSecret { get; set; }
+		#endregion
+	}
+}
