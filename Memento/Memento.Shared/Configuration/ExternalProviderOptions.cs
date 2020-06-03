bbb2e06ee97793060cfa4d9634b57486855ef0c8
Tiago Reis <nuno-tiago-reis@outlook.pt>
2020-06-03 17:15:06 +0100

    IdentityServerOptions:
    Added the IdentityServerClientOptions class.
    Added the IdentityServerResourceOptions class.

--- Memento/Memento.Shared/Configuration/ExternalProviderOptions.cs
+++ Memento/Memento.Shared/Configuration/ExternalProviderOptions.cs
@@ -1,19 +1,24 @@
-﻿namespace Memento.Shared.Configuration
+﻿using JetBrains.Annotations;
+
+namespace Memento.Shared.Configuration
 {
 	/// <summary>
 	/// Implements the 'ExternalProvider' options.
 	/// </summary>
+	[UsedImplicitly]
 	public sealed class ExternalProviderOptions
 	{
 		#region [Properties]
 		/// <summary>
 		/// Gets or sets the api key.
 		/// </summary>
+		[UsedImplicitly]
 		public string ApiKey { get; set; }
 
 		/// <summary>
 		/// Gets or sets the api secret.
 		/// </summary>
+		[UsedImplicitly]
 		public string ApiSecret { get; set; }
 		#endregion
 	}
---
+++ Memento/Memento.Shared/Configuration/IdentityServerResourceOptions.cs
@@ -0,0 +1,25 @@
+﻿using JetBrains.Annotations;
+
+namespace Memento.Shared.Configuration
+{
+	/// <summary>
+	/// Implements the 'IdentityServerResource' options.
+	/// </summary>
+	[UsedImplicitly]
+	public sealed class IdentityServerResourceOptions
+	{
+		#region [Properties]
+		/// <summary>
+		/// Gets or sets the authority.
+		/// </summary>
+		[UsedImplicitly]
+		public string Authority { get; set; }
+
+		/// <summary>
+		/// Gets or sets the audience.
+		/// </summary>
+		[UsedImplicitly]
+		public string Audience { get; set; }
+		#endregion
+	}
+}
